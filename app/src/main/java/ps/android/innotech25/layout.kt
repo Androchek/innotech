c8738e68e93785ef137c16f1340429aa3b44d338
Pratham Soni <prathamsoni543@gmail.com>
2024-11-17 20:25:38 +0530

    initial commit

---
+++ app/src/main/java/ps/android/innotech25/layout.kt
@@ -0,0 +1,18 @@
+package ps.android.innotech25
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class layout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_layout)
+
+
+
+    }
+}
---
+++ app/src/main/java/ps/android/innotech25/sign_up.kt
@@ -0,0 +1,22 @@
+package ps.android.innotech25
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class sign_up : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sign_up)
+
+
+
+
+
+
+
+    }
+}
